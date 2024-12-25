6539ba447dabab7af0c7494d3e7caac80827f48e
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-25 22:44:03 +0300

    End of Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import {
   FaFacebookF,
   FaInstagram,
@@ -6,8 +8,10 @@ import {
 } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { MdOutlineEmail, MdOutlinePhone } from "react-icons/md";
+import { TbPhoneCall } from "react-icons/tb";
 
 export const Header = () => {
+  const links = ["Ana Sayfa", "Hakkımızda", "Hizmetlerimiz", "İletişim"];
   return (
     <header className="bg-primary h-14">
       <div className="container h-full flex items-center">
@@ -39,8 +43,32 @@ export const Header = () => {
               size={18}
               className="cursor-pointer hover:opacity-70 transition duration-300"
             />
-            <div className="h-14 text-xl bg-whatsapp flex flex-row gap-2 justify-center items-center px-4 text-black hover:brightness-75 transition duration-300 cursor-pointer">
-              <FaWhatsapp /> WhatsApp
+            <div className="h-14 text-lg bg-whatsapp flex flex-row gap-2 justify-center items-center px-7 text-black hover:brightness-75 transition duration-300 cursor-pointer">
+              <FaWhatsapp size={24}/> WhatsApp
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="shadow-2xl">
+        <div className="container flex justify-between items-center py-4">
+          <div>
+            <Image src="/logo.png" alt="logo" width={140} height={40} />
+          </div>
+          <div className="flex flex-row gap-6">
+            {links.map((x: string) => (
+              <Link
+                href="#"
+                className="font-bold text-sm text-primary hover:opacity-50 transition duration-300"
+              >
+                {x}
+              </Link>
+            ))}
+          </div>
+          <div className="flex flex-row gap-3 items-center">
+            <TbPhoneCall size={48} className="border border-black/10 shadow-lg shadow-blas p-1.5 rounded-full"/>
+            <div>
+                <h2 className="ms-2">Call Now</h2>
+                <h5 className="font-medium text-lg">+90 542 542 5422</h5>
             </div>
           </div>
         </div>
