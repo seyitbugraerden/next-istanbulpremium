84e2a23fea5307d25d5d58c2dfc878b22cbce8c6
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-27 00:39:05 +0300

    service fixes

--- src/components/Home/AboutUs.tsx
+++ src/components/Home/AboutUs.tsx
@@ -4,53 +4,55 @@ import { FaCheckDouble } from "react-icons/fa";
 
 export const AboutUs = () => {
   return (
-    <section className="grid grid-cols-5 container gap-12 mt-56 pb-12">
-      <div className="col-span-2 relative">
-        <Image
-          src="https://picsum.photos/200/300"
-          alt="About Image"
-          width={340}
-          height={340}
-          className="w-full h-[550px] object-cover"
-        />
-        <div className="absolute -top-6 -left-6 bg-red-500 p-4 flex justify-center items-center flex-col">
-          <span className="text-4xl font-bold text-white">25</span>
-          <span className="text-white font-bold">Years of</span>
-          <span className="text-white font-bold">Experience</span>
+    <div className="h-dvh flex justify-center items-center">
+      <section className="grid grid-cols-5 container gap-12">
+        <div className="col-span-2 relative">
+          <Image
+            src="https://picsum.photos/200/300"
+            alt="About Image"
+            width={340}
+            height={340}
+            className="w-full h-[550px] object-cover"
+          />
+          <div className="absolute -top-6 -left-6 bg-red-500 p-4 flex justify-center items-center flex-col">
+            <span className="text-4xl font-bold text-white">25</span>
+            <span className="text-white font-bold">Years of</span>
+            <span className="text-white font-bold">Experience</span>
+          </div>
         </div>
-      </div>
-      <div className="col-span-3 flex flex-col gap-4">
-        <h6 className="text-red-500 text-sm font-bold">HAKKIMIZDA</h6>
-        <h2 className="text-black text-6xl font-bold">
-          Make your car feel like a brand new one
-        </h2>
-        <p className="text-lg font-light text-justify">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
-          rem amet veniam itaque ea nihil reprehenderit! Eius qui blanditiis
-          aspernatur dolores earum facilis libero dolorem, odio deserunt
-          molestias fugiat vel.earum facilis libero dolorem, odio deserunt
-          molestias fugiat vel.
-        </p>
-        <p className="text-lg font-light text-justify">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
-          rem amet veniam itaque ea nihil reprehenderit! Eius qui blanditiis
-          aspernatur dolores earum facilis libero dolorem, odio deserunt
-          molestias fugiat vel.earum facilis libero dolorem, odio deserunt
-          molestias fugiat vel.
-        </p>
-        <div className="flex flex-row items-center gap-2">
-          <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum dolor
-          sit amet.
+        <div className="col-span-3 flex flex-col gap-4">
+          <h6 className="text-red-500 text-sm font-bold">HAKKIMIZDA</h6>
+          <h2 className="text-black text-6xl font-bold">
+            Make your car feel like a brand new one
+          </h2>
+          <p className="text-lg font-light text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
+            rem amet veniam itaque ea nihil reprehenderit! Eius qui blanditiis
+            aspernatur dolores earum facilis libero dolorem, odio deserunt
+            molestias fugiat vel.earum facilis libero dolorem, odio deserunt
+            molestias fugiat vel.
+          </p>
+          <p className="text-lg font-light text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
+            rem amet veniam itaque ea nihil reprehenderit! Eius qui blanditiis
+            aspernatur dolores earum facilis libero dolorem, odio deserunt
+            molestias fugiat vel.earum facilis libero dolorem, odio deserunt
+            molestias fugiat vel.
+          </p>
+          <div className="flex flex-row items-center gap-2">
+            <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum
+            dolor sit amet.
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum
+            dolor sit amet.
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum
+            dolor sit amet.
+          </div>
         </div>
-        <div className="flex flex-row items-center gap-2">
-          <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum dolor
-          sit amet.
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <FaCheckDouble className="text-red-500" size={12} /> Lorem ipsum dolor
-          sit amet.
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
--- src/components/Home/ServiceTop.tsx
+++ src/components/Home/ServiceTop.tsx
@@ -4,52 +4,56 @@ import { MdOutlineCarRental } from "react-icons/md";
 
 export const ServiceTop = () => {
   return (
-    <div className="container pb-16">
-      <h6 className="text-red-500 text-sm font-bold text-center">
-        Servislerimiz
-      </h6>
-      <h2 className="text-black text-4xl font-bold text-center pt-2">
-        Ne Sunuyoruz?
-      </h2>
-      <div className="flex gap-6 mt-12">
-        <div className="flex-1 flex flex-col justify-between">
-          {Array.from({ length: 3 }).map(() => (
-            <div className="p-6 flex flex-row items-start gap-6 bg-white border shadow-sm shadow-black/30">
-              <MdOutlineCarRental size={64} className="text-red-500" />
+    <div className="h-[80dvh] flex justify-center items-center">
+      <div className="container -translate-y-6">
+        <h6 className="text-red-500 text-sm font-bold text-center">
+          Servislerimiz
+        </h6>
+        <h2 className="text-black text-4xl font-bold text-center pt-2">
+          Ne Sunuyoruz?
+        </h2>
+        <div className="flex gap-6 mt-12">
+          <div className="flex-1 flex flex-col justify-between">
+            {Array.from({ length: 3 }).map(() => (
+              <div className="p-6 flex flex-row items-start gap-6 bg-white border shadow-sm shadow-black/30">
+                <MdOutlineCarRental size={64} className="text-red-500" />
 
-              <div>
-                <h2 className="text-base font-semibold mb-2">Arıza Tespit</h2>
-                <p className="text-xs font-thin">
-                  Gelişmiş arıza tespit cihazları ile araçlardaki elektronik ve
-                  mekanik sorunların hızlı ve doğru bir şekilde belirlenmesi
-                </p>
+                <div>
+                  <h2 className="text-base font-semibold mb-2">Arıza Tespit</h2>
+                  <p className="text-xs font-thin">
+                    Gelişmiş arıza tespit cihazları ile araçlardaki elektronik
+                    ve mekanik sorunların hızlı ve doğru bir şekilde
+                    belirlenmesi
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex-1">
-          <Image
-            src="/item.jpg"
-            alt="About Image"
-            width={340}
-            height={340}
-            className="aspect-square w-full"
-          />
-        </div>
-        <div className="flex-1 flex flex-col justify-between">
-          {Array.from({ length: 3 }).map(() => (
-            <div className="p-6 flex flex-row items-start gap-6 bg-white border shadow-sm shadow-black/30">
-              <MdOutlineCarRental size={64} className="text-red-500" />
+            ))}
+          </div>
+          <div className="flex-1">
+            <Image
+              src="/item.jpg"
+              alt="About Image"
+              width={340}
+              height={340}
+              className="aspect-square w-full"
+            />
+          </div>
+          <div className="flex-1 flex flex-col justify-between">
+            {Array.from({ length: 3 }).map(() => (
+              <div className="p-6 flex flex-row items-start gap-6 bg-white border shadow-sm shadow-black/30">
+                <MdOutlineCarRental size={64} className="text-red-500" />
 
-              <div>
-                <h2 className="text-base font-semibold mb-2">Arıza Tespit</h2>
-                <p className="text-xs font-thin">
-                  Gelişmiş arıza tespit cihazları ile araçlardaki elektronik ve
-                  mekanik sorunların hızlı ve doğru bir şekilde belirlenmesi
-                </p>
+                <div>
+                  <h2 className="text-base font-semibold mb-2">Arıza Tespit</h2>
+                  <p className="text-xs font-thin">
+                    Gelişmiş arıza tespit cihazları ile araçlardaki elektronik
+                    ve mekanik sorunların hızlı ve doğru bir şekilde
+                    belirlenmesi
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
