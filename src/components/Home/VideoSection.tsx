02542d4968f474ed55e565cb820962760cb50e79
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-18 23:07:29 +0300

    video control

--- src/components/Home/VideoSection.tsx
+++ src/components/Home/VideoSection.tsx
@@ -8,14 +8,14 @@ const VideoSection = () => {
   if (isVideoValue) {
     return (
       <section className="fixed top-0 left-0 flex justify-center items-center w-full h-full z-[999] bg-black/60">
-        <div className="relative">
+        <div className="relative max-w-[60dvw] lg:max-w-full">
           <div
             onClick={() => {
               setIsVideoValue(false);
             }}
             className="cursor-pointer"
           >
-            <IoMdClose className=" absolute top-0 -right-16 size-8 text-black bg-white rounded-lg p-1" />
+            <IoMdClose className=" absolute -top-6 lg:top-0 -right-10 lg:-right-16 size-8 text-black bg-white rounded-lg p-1" />
           </div>
           <video controls>
             <source src="/video.mp4" type="video/mp4" />
